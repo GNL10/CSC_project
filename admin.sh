cefaec7872eb100d5f0e595694586eb37334af1b
gnl10 <goncalo_mao_cheia@hotmail.com>
2020-11-22 21:53:10 +0000

    added server and cert auth certificates

---
+++ admin.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+admin_pwd="admin"
+
+#folders
+admin="admin"
+server="server"
+
+#certificates and keys
+cert_auth_key="my-ca.key"
+cert_auth_crt="my-ca.crt"
+
+server_pem="csc-server.pem"
+server_csr="csc-server.csr"
+server_crt="csc-server.crt"
+
+#creating folders
+mkdir $admin
+mkdir $server
+
+#creation of a certificate authority
+cd $admin
+openssl genrsa -des3 -passout pass:$admin_pwd -out $cert_auth_key 2048
+openssl req -new -x509 -days 3650 -key $cert_auth_key -passin pass:$admin_pwd -out $cert_auth_crt -subj "/C=PT/ST=Lisbon/L=Lisbon/O=CSC-21/OU=CA-21/CN=CA21"
+cd ..
+
+#creating of a certificate for the server
+cd $server
+openssl genrsa -out $server_pem 1024
+openssl req -new -key $server_pem -out $server_csr -subj "/C=PT/ST=Lisbon/L=Lisbon/O=CSC-21/OU=Server/CN=Server"
+    
+#signing the server certificate
+cp $server_csr ../$admin
+cd ../$admin 
+openssl x509 -req -in $server_csr -out $server_crt -sha1 -CA $cert_auth_crt -CAkey $cert_auth_key -passin pass:$admin_pwd -CAcreateserial -days 3650
+cp $server_crt ../$server
+cp $cert_auth_crt ../$server
+cd ..
+
+#
