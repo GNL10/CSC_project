f2f93afc2fa29688769647a2122cd03fb164ed75
gnl10 <goncalo_mao_cheia@hotmail.com>
2020-12-11 01:06:45 +0000

    client working

--- client.cpp
+++ client.cpp
@@ -2,7 +2,18 @@
 
 char const *PK_fname = "public_key.txt";
 char const *SK_fname = "secret_key.txt";
-char const *input_fname  = "fhe_encrypted.txt";
+
+char const *fhe_out_fname  = "fhe_to_server.txt";
+char const *fhe_in_fname  = "fhe_to_server.txt"; //"fhe_from_server.txt"; CHANGE TO THIS WHEN SERVER IS WORKING!!!
+
+char const *cmd_out_fname = "cmds_out_enc.txt";
+char const *cmd_in_fname = "cmds_out_enc.txt"; //"cmds_in_enc.txt"; CHANGE TO THIS WHEN SERVER IS WORKING!!!
+
+char const *int_placeholder = "_int_";
+
+void delete_char_in_str (string &str, char c);
+void send_command (SEALContext context, PublicKey public_key, ofstream &cmd_file_out, ofstream &fhe_file_out);
+void read_command (SEALContext context, SecretKey secret_key, ifstream &cmd_file_in, ifstream &fhe_file_in);
 
 int main () {
 
@@ -12,26 +23,29 @@ int main () {
     PublicKey public_key = load_PK_from_file(context, PK_fname);
     SecretKey secret_key = load_SK_from_file(context, SK_fname);
 
-    //open 
-    ofstream encrypted_file_out;
-    encrypted_file_out.open(input_fname, ios::binary);
+    // files to write to
+    ofstream fhe_file_out, cmd_file_out;
+    fhe_file_out.open(fhe_out_fname, ios::binary);
+    cmd_file_out.open(cmd_out_fname, ios::binary);
 
-    ifstream encrypted_file_in;
-	encrypted_file_in.open(input_fname);
-
+    // files to read from
+    ifstream fhe_file_in, cmd_file_in;
+	fhe_file_in.open(fhe_in_fname, ios::binary);
+    cmd_file_in.open(cmd_in_fname, ios::binary);
 
     while (flag) {
         string input;
 
-        cout << "\n\nPlease enter an integer value: " << endl;
         cout << "\nThis is the Options Menu:\n" << endl;
         cout << "- PRESS 1 to create a security key;" << endl;
         cout << "- PRESS 2 to send a command;" << endl;
         cout << "- PRESS 3 to read responses." << endl;
         cout << "- PRESS 4 to quit." << endl;
 
-        cin >> input;
-        int i;
+        
+        getline(cin, input, '\n');
+        
+        int i = 0;
         std::istringstream(input) >> i;
         
         switch (i) {
@@ -39,10 +53,10 @@ int main () {
                 gen_keys(context, SK_fname, PK_fname);
                 break;
             case 2:
-                encrypt_value(context, 3, public_key, encrypted_file_out);
+                send_command(context, public_key, cmd_file_out, fhe_file_out);                
                 break;
             case 3:
-                cout << "Decrypted value : " << decrypt_value(context, secret_key, encrypted_file_in);
+                read_command(context, secret_key, cmd_file_in, fhe_file_in);
                 break;
             case 4:
                 flag = false;
@@ -53,6 +67,67 @@ int main () {
         }
     }
 
-    encrypted_file_out.close();
-    encrypted_file_in.close();
+    fhe_file_out.close();
+    cmd_file_out.close();
+    fhe_file_in.close();
+    cmd_file_in.close();
+}
+
+
+void delete_char_in_str (string &str, char c) {
+    size_t pos = str.find(c);
+    if (pos != std::string::npos)
+        str.replace(pos, 1, "");
+}
+
+void send_command (SEALContext context, PublicKey public_key, ofstream &cmd_file_out, ofstream &fhe_file_out) {
+    string str; 
+    stringstream str_strm;
+    int temp_int;
+    string temp_str;
+    size_t pos = 0;
+
+    cout << "Enter command:" << endl;
+    getline(cin, str, ';');    
+    
+    
+    str_strm << str; //convert the string s into stringstream
+
+    while(!str_strm.eof()) {
+        str_strm >> temp_str; //take words into temp_str one by one
+        
+        delete_char_in_str(temp_str, '(');
+        delete_char_in_str(temp_str, ',');
+        delete_char_in_str(temp_str, ')');
+
+        if(stringstream(temp_str) >> temp_int) { //try to convert string to int
+            encrypt_value(context, temp_int, public_key, fhe_file_out); 
+            
+            pos = str.find(to_string(temp_int));
+            str.replace(pos, to_string(temp_int).length(), int_placeholder);
+        }
+        temp_str = ""; //clear temp string
+    }
+
+    cmd_file_out << str << endl;
+    if (DEBUG)
+        cout << "[DEBUG] Command after removing ints: " << str << endl;
+    getline(cin, str, '\n'); // ignore the trash until the \n
+}
+
+void read_command (SEALContext context, SecretKey secret_key, ifstream &cmd_file_in, ifstream &fhe_file_in) {
+    string line;
+    size_t pos = 0;
+    int val;
+
+    while ( getline (cmd_file_in,line) ) { //run through all lines of file
+        if (DEBUG)
+            cout << "[DEBUG] Command before changing int placeholders: " << line << endl;
+
+        while ((pos = line.find(int_placeholder)) != string::npos ) { // for each placeholder decrypt int
+            val = decrypt_value(context, secret_key, fhe_file_in);
+            line.replace(pos, strlen(int_placeholder), to_string(val));
+        }
+        cout << line << endl;
+    }
 }
